226378f46c307c4d80de8a5b7af6abafa6abef4b
ermineaweb <ermineaweb@gmail.com>
2020-05-09 00:00:00 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tartufe
+
+Jeu de société node / react / graphql.
---
+++ client/webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].[contenthash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"],
+                        plugins: ["@babel/plugin-transform-runtime"]
+                    },
+                },
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx'
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: require('html-webpack-template'),
+            inject: true,
+            appMountId: 'root',
+            filename: 'index.html'
+        }),
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
+};
