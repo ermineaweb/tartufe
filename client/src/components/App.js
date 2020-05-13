aec1ff8374d59a16a8db7801aed43764b24bd51a
ermineaweb <ermineaweb@gmail.com>
2020-05-13 22:32:48 +0200

    v0.1

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,11 +11,11 @@ import {getMainDefinition} from "apollo-utilities";
 import UserContextProvider, {UserContext} from "../context";
 
 const httpLink = new HttpLink({
-    uri: 'http://http://51.91.97.13/:4000/graphql'
+    uri: 'http://51.91.97.13/:4000/graphql'
 });
 
 const wsLink = new WebSocketLink({
-    uri: 'ws://http://51.91.97.13/:4000/graphql',
+    uri: 'ws://51.91.97.13/:4000/graphql',
     options: {
         reconnect: true,
     }
